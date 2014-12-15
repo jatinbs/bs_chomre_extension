e9d26625a32a02c82fcd090ba516b0a99c2158df
jatin <jatin@browserstack.com>
2014-12-15 19:37:44 +0530

    refactored most of the code. enabled mobile toggle button functionality. opens site url.

--- app/assets/js/popup.js
+++ app/assets/js/popup.js
@@ -1,13 +1,41 @@
 jQuery(document).ready(function() {
-  var operatingSystemsSelect2 = [];
-  var browsersSelect2 = [];
+
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    jQuery('#url-select').val(tabs[0].url);
+  });
+
+
+    //selectors
+  var browserSelect = jQuery('#browser-select');
+  var osSelect = jQuery('#os-select');
+
+  //init all global vars
+  var osSelectData = [];
+  var browserSelectData = [];
   var globalData = null;
   var deviceType = jQuery('input[name=device-type]:checked').val();
 
+  var type = (deviceType == 'desktop') ? 'browsers' : 'devices';
 
-  jQuery('#browser-select').select2({
-    data: browsersSelect2
+  //init both select2
+  browserSelect.select2({
+    data: browserSelectData
   }).select2('enable', false);
+
+  osSelect.select2({
+    data: osSelectData
+  }).select2('enable', false);
+
+  //change listener for os so browsers/devices can be loaded
+  jQuery('input[name=device-type]').change(function(e) {
+    deviceType = jQuery('input[name=device-type]:checked').val();
+    loadOS();
+    browserSelectData = [];
+    browserSelect.select2({
+      data: browserSelectData
+    }).select2('enable', false);
+
+  });
 
   //get browsers list
   jQuery.ajax({
@@ -15,33 +43,9 @@ jQuery(document).ready(function() {
     datatype: 'json',
     success: function(response) {
       globalData = response;
-      console.log(deviceType);
-      globalData[deviceType];
-      for(var i in globalData[deviceType]) {
-        console.log(i);
-        operatingSystemsSelect2.push({
-          id: i,
-          text: globalData[deviceType][i].os_display_name
-        })
-      }
-      jQuery('#os-select').select2({
-        data: operatingSystemsSelect2
-      }).change(function(e) {
-
-        //set browser/device select
-        browsersSelect2 = [];
-        for(var i in globalData[deviceType][e.val].browsers) {
-          browsersSelect2.push({
-            id: i,
-            text: globalData[deviceType][e.val].browsers[i].display_name
-          });
-        }
-        jQuery('#browser-select').select2('data', {});
-        jQuery('#browser-select').select2({
-          data: browsersSelect2
-        });
-        jQuery('#browser-select').select2('enable', true);
-
+      loadOS();
+      osSelect.change(function(e) {
+        loadBrowsers(e.val);
       });
 
     },
@@ -49,4 +53,65 @@ jQuery(document).ready(function() {
 
     }
   });
+
+  //load the operating systems
+  var loadOS = function() {
+    osSelectData = [];
+    for(var i in globalData[deviceType]) {
+      if(globalData[deviceType].hasOwnProperty(i)) {
+        osSelectData.push({
+          id: i,
+          text: globalData[deviceType][i]['os_display_name']
+        })
+      }
+    }
+
+    osSelect.select2('data', {});
+    osSelect.select2({
+      data: osSelectData
+    });
+    osSelect.select2('enable', true);
+  };
+
+  //load browsers depending on operating system selected.
+  var loadBrowsers = function(index) {
+    browserSelectData = [];
+    type = (deviceType == 'desktop') ? 'browsers' : 'devices';
+    for(var i in globalData[deviceType][index][type]) {
+      if(globalData[deviceType][index][type].hasOwnProperty(i)) {
+        browserSelectData.push({
+          id: i,
+          text: globalData[deviceType][index][type][i]['display_name']
+        });
+      }
+    }
+    browserSelect.select2('data', {});
+    browserSelect.select2({
+      data: browserSelectData
+    });
+    browserSelect.select2('enable', true);
+  };
+
+
+  jQuery('#open-bs').click(function(e) {
+    var bs_params = {
+      os: "Windows",
+      os_version: "7",
+      browser: "IE",
+      browser_version: "8.0",
+      zoom_to_fit: true,
+      full_screen: true,
+      autofit: true,
+      url: jQuery('#url-select').val(),
+      resolution: "2048x1536",
+      speed: 1,
+      start: true
+    }
+
+    newURL = 'http://browserstack.com/start#' + jQuery.param(bs_params);
+    chrome.tabs.create({ url: newURL });
+    e.preventDefault();
+  });
+
+
 });
